e50d841a5468c011126db5e8ee8d210589d58d65
Taskim <miksa_t@etna-alternance.net>
2020-09-06 15:50:20 +0200

    Updates

---
+++ precache-manifest.60863aa4d4d560a683be66fea7f38ab1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b35dca137526fe6d255ac7d3ed7cd2",
+    "url": "/book/index.html"
+  },
+  {
+    "revision": "e668d489eceb90301d9c",
+    "url": "/book/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6fa5dee9b795b85ff213",
+    "url": "/book/static/js/2.7333f71b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/book/static/js/2.7333f71b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e668d489eceb90301d9c",
+    "url": "/book/static/js/main.bd86e608.chunk.js"
+  },
+  {
+    "revision": "dcb417c46ea1a3330201",
+    "url": "/book/static/js/runtime-main.a81774fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/book/static/media/logo.5d5d9eef.svg"
+  }
+]);
