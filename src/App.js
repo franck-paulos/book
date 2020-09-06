426f9c9f6d82f55263957cb28b5a1e4d645ceafb
Taskim <miksa_t@etna-alternance.net>
2020-09-06 17:31:30 +0200

    feat(app): add scroll snap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
+
+import cerf from './assets/cerf.svg'
+import s from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Franck Paulos</h1>
+    <div className={s.app}>
+      <div style={{ backgroundColor: 'dodgerblue' }} className={s.page}>
+        <div className={s.name}>Franck Paulos</div>
+      </div>
+      <div style={{ backgroundColor: 'hotpink' }} className={s.page}>
+        <img src={cerf} alt="cerf" />
+      </div>
+      <div style={{ backgroundColor: 'mediumspringgreen' }} className={s.page}>
+        <img src={cerf} alt="cerf" />
+      </div>
+      <div style={{ backgroundColor: 'lightsalmon' }} className={s.page}>
+        <img src={cerf} alt="cerf" />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
