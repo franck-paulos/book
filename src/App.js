c072c47b921e13f5ba04ef7b91cae8678fc1ad7d
franck-paulos <franckpaulos@hotmail.fr>
2020-09-20 18:51:41 +0200

    add gameboy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import cerf from './assets/cerf.svg'
+import gameboy from './assets/Gameboy.svg'
 import s from './App.module.css'
 
 function App() {
@@ -18,14 +19,15 @@ function App() {
           de la vie éternelle.
         </div>
       </div>
-      <div style={{ backgroundColor: 'mediumspringgreen' }} className={s.page}>
+      <div style={{ backgroundColor: '#7700faa3' }} className={s.page}>
         <div className={s.description}>
-          Si les rois ont voulu être inhumés dans une peau de cerf nappant leur
-          dépouille mortelle, c’est qu’elle a la vertu, elle qui vivante
-          ressuscitait chaque année par ses bois, de montrer à l’âme le chemin
-          de la vie éternelle.
+          La Game Boy est une console portable de jeu vidéo 8-bits de quatrième
+          génération développée et fabriquée par Nintendo. Mise en vente au
+          Japon le 21 avril 1989, puis en Amérique du Nord en octobre 1989, et
+          enfin en Europe le 28 septembre 1990, elle est la première console
+          portable de la gamme des Game Boy
         </div>
-        <img src={cerf} alt="cerf" />
+        <img src={gameboy} alt="gameboy" />
       </div>
       <div style={{ backgroundColor: 'lightsalmon' }} className={s.page}>
         <img src={cerf} alt="cerf" />
