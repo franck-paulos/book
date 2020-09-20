22e61223e13acab06cc27642609b73f468738cfb
Taskim <miksa_t@etna-alternance.net>
2020-09-20 18:30:58 +0200

    feat(slide): add description

--- src/App.js
+++ src/App.js
@@ -11,12 +11,30 @@ function App() {
       </div>
       <div style={{ backgroundColor: 'hotpink' }} className={s.page}>
         <img src={cerf} alt="cerf" />
+        <div className={s.description}>
+          Si les rois ont voulu être inhumés dans une peau de cerf nappant leur
+          dépouille mortelle, c’est qu’elle a la vertu, elle qui vivante
+          ressuscitait chaque année par ses bois, de montrer à l’âme le chemin
+          de la vie éternelle.
+        </div>
       </div>
       <div style={{ backgroundColor: 'mediumspringgreen' }} className={s.page}>
+        <div className={s.description}>
+          Si les rois ont voulu être inhumés dans une peau de cerf nappant leur
+          dépouille mortelle, c’est qu’elle a la vertu, elle qui vivante
+          ressuscitait chaque année par ses bois, de montrer à l’âme le chemin
+          de la vie éternelle.
+        </div>
         <img src={cerf} alt="cerf" />
       </div>
       <div style={{ backgroundColor: 'lightsalmon' }} className={s.page}>
         <img src={cerf} alt="cerf" />
+        <div className={s.description}>
+          Si les rois ont voulu être inhumés dans une peau de cerf nappant leur
+          dépouille mortelle, c’est qu’elle a la vertu, elle qui vivante
+          ressuscitait chaque année par ses bois, de montrer à l’âme le chemin
+          de la vie éternelle.
+        </div>
       </div>
     </div>
   )
